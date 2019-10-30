275775a0147de3592242ca7d4027c232ba8d8065
Jordan <jordandoan@hotmail.com>
2019-10-29 23:01:13 -0700

    Added middleware to events router, cleaned up code

---
+++ api/events/events-middleware.js
@@ -0,0 +1,43 @@
+const helpers = require("../../helpers");
+const Events = require("./events-model");
+
+module.exports = {
+  validateEvent,
+  findEvent,
+  isOwner
+}
+
+function validateEvent(req,res,next) {
+  const {date, title, points} = req.body;
+  const event = {date, title, points};
+  if (event) {
+    if (event.title && event.date && event.points) {
+      req.event = event;
+      next();
+    } else {
+      helpers.errorMsg(res, 400, "Title, date, and value is required.")
+    }
+  } else {
+    helpers.errorMsg(res, 400, "Body is required.")
+  }
+}
+
+function findEvent(req,res,next) {
+  Events.getById(req.params.id)
+    .then(event => {
+      if (event) {
+        req.found = event;
+        next();
+      } else {
+        helpers.errorMsg(res, 404, "Event with specified ID not found.");
+      }
+    })
+    .catch(err => helpers.errorMsg(res, 500, "Error accessing database."));
+}
+function isOwner(req,res,next) { 
+  if (req.found.user_id == req.decoded.id) {
+    next();
+  } else {
+    helpers.errorMsg(res, 403, "You are not authorized to perform this action.");
+  }
+}
