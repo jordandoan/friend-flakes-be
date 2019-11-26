7ef10f91bfa41b793f5f3bd42ff36a85068d4d2f
Jordan <jordandoan@hotmail.com>
2019-11-26 09:21:28 -0800

    Preparing for production deployment

--- knexfile.js
+++ knexfile.js
@@ -42,20 +42,19 @@ module.exports = {
     },
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
 
 };
--- secrets/index.js
+++ secrets/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'chickennugget',
+  jwtSecret: process.env.JWT_SECRET,
 };
